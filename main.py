56da93ef90c5f995eb8222247adfb1f3ab8053b8
Brant Drayer <brantdrayer@gmail.com>
2025-05-05 19:52:05 +0000

    changed to joke finder

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+@app.get("/joke")
+def get_joke():
+    url = "https://icanhazdadjoke.com/"
+    headers = {"Accept": "text/plain"}
+
+    response = httpx.get(url, headers=headers)
+    joke = response.text.strip()
+
+    return {"joke": joke}
+
